e0b32aeae766acc4ef9d845463a41a11f4979cd7
sorhan <sorhan3.14@gmail.com>
2023-04-04 17:16:18 +0300

    move image to public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <img className="profile-img" src="src/assets/laura-smith.png" alt="Laura Smith's photo" />
+        <img className="profile-img" src="/images/laura-smith.png" alt="Laura Smith's photo" />
     )
 }
